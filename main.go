ed6bdc90822734cfb8a200a7ffb9b27e2e577dbc
Nolan Brubaker <nolan@heptio.com>
2018-07-14 16:41:30 -0400

    Look for redefinitions before merging
    Signed-off-by: Nolan Brubaker <nolan@heptio.com>

--- main.go
+++ main.go
@@ -12,21 +12,51 @@ import (
 
 func main() {
 
-	files := os.Args[1:]
+	if len(os.Args) < 3 {
+		log.Fatalf("Need at least 2 files to merge")
+		return
+	}
+
+	files := os.Args[2:]
 	if len(files) < 2 {
 		log.Fatalf("Need at least 2 files to merge")
 	}
 
 	var merged []*types.Entry
+	redefs := make(map[string][]*types.Entry)
+
 	for i := 0; i < len(files); i++ {
-		e, err := file.CSVToEntries(files[i])
+		// Load the entries
+		es, err := file.CSVToEntries(files[i])
 		if err != nil {
 			log.Fatalf("Error with file %s: %s", files[i], err)
 		}
-		merged = entries.Merge(merged, e)
+
+		// Look for redefinitions
+		for _, e := range es {
+			rds, ok := entries.FindRedefinition(e, merged)
+			if ok {
+				key := fmt.Sprintf("%s:%s", files[i], e.ToString())
+				redefs[key] = rds
+			}
+		}
+		merged = entries.Merge(merged, es)
+	}
+
+	if len(redefs) > 0 {
+		fmt.Println("Redefintions were found, can't merge")
+		for key, vals := range redefs {
+			fmt.Println(key)
+			for _, v := range vals {
+				fmt.Printf("\t%s\n", v.ToString())
+			}
+		}
+		fmt.Println("Redefintions were found, can't merge")
+		return
 	}
 
 	for _, m := range merged {
 		fmt.Println(m.ToString())
 	}
+
 }
